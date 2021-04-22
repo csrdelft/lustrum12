d7ed09cb3419e3221d0ab1489891ec9e14b3d29a
Job Kanis <jobkanis@gmail.com>
2021-04-22 17:01:25 +0200

    Themabekendmaking Lustrum Stek

--- src/main.js
+++ src/main.js
@@ -9,3 +9,25 @@ window.addEventListener('load', function () {
     document.getElementById("player").innerHTML =
         `<iframe src="https://www.youtube-nocookie.com/embed/9rXmPGNczdE?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 });
+
+window.onscroll = function(){
+    var scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    var vertical_position = pageYOffset || document.documentElement.scrollTop;
+    var percentile = (vertical_position / scrollMaxY)
+
+    var zandloper = document.getElementById("zandloper");
+    var heel = document.getElementById("zandloper_heel");
+    var kapot = document.getElementById("zandloper_kapot");
+
+    zandloper.style.top = percentile * 100 + '%';
+    zandloper.style.marginTop = -50 - (20 * percentile) + "px";
+
+    if (percentile < 0.99) {
+        heel.style.display = "";
+        kapot.style.display = "none";
+    } else {
+        heel.style.display = "none";
+        kapot.style.display = "";
+    }
+
+};
