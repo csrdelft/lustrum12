770a09871c8a2481725cc5e2a5c07c03e241fe45
Job Kanis <jobkanis@gmail.com>
2021-05-23 15:22:51 +0200

    Verplaats zandloper.js en plaatjes passen op klein scherm

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,23 +1,2 @@
 import './home.scss'
-
-window.onscroll = function(){
-    var scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
-    var vertical_position = pageYOffset || document.documentElement.scrollTop;
-    var percentile = (vertical_position / scrollMaxY)
-
-    var zandloper = document.getElementById("zandloper");
-    var heel = document.getElementById("zandloper_heel");
-    var kapot = document.getElementById("zandloper_kapot");
-
-    zandloper.style.top = percentile * 100 + '%';
-    zandloper.style.marginTop = -50 - (20 * percentile) + "px";
-
-    if (percentile < 0.99) {
-        heel.style.display = "";
-        kapot.style.display = "none";
-    } else {
-        heel.style.display = "none";
-        kapot.style.display = "";
-    }
-
-};
+import '../../assets/zandloper.js'
