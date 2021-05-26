8d178e86945e96e42baff0ea258c2f91f5fe57ed
Job Kanis <jobkanis@gmail.com>
2021-05-26 12:46:42 +0200

    Put publicPath back

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,25 @@
 module.exports = {
+    publicPath: '.',
     pages: {
         index: {
             entry: 'src/pages/home/home.js',
-            template: 'src/pages/home/home.html'
+            template: 'src/pages/home/home.html',
+            filename: 'home.html'
         },
         opening: {
             entry: 'src/pages/opening/opening.js',
-            template: 'src/pages/opening/opening.html'
+            template: 'src/pages/opening/opening.html',
+            filename: 'opening.html'
         },
         lustrumweek: {
             entry: 'src/pages/lustrumweek/lustrumweek.js',
-            template: 'src/pages/lustrumweek/lustrumweek.html'
+            template: 'src/pages/lustrumweek/lustrumweek.html',
+            filename: 'lustrumweek.html'
         },
         thema: {
             entry: 'src/pages/thema/thema.js',
-            template: 'src/pages/thema/thema.html'
+            template: 'src/pages/thema/thema.html',
+            filename: 'thema.html'
         }
     }
 };
